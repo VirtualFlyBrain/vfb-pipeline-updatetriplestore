a34aabee79329b30bffd41b761ad8a7812b736bd
matentzn <nicolas.matentzoglu@gmail.com>
2018-06-29 17:37:16 +0100

    initial

---
+++ process.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Set up VFB"
+VFBSETUP=${WORKSPACE}/rdf4j_vfb.txt
+RDF4J=/opt/eclipse-rdf4j-${RDF4J_VERSION}
+RDF4JSERVER=${SERVER}/rdf4j-server
+DATA=/data
+
+echo "connect "${RDF4JSERVER}|cat - ${VFBSETUP} > /tmp/out && mv /tmp/out ${VFBSETUP}
+cat ${VFBSETUP}
+cat ${VFBSETUP} | sh ${RDF4J}/bin/console.sh
+
+ls -l $DATA
+
+cd $DATA
+
+# The following for loop writes the load commands into the RDF4J setup script
+for i in *.ttl.gz; do
+    [ -f "$i" ] || break
+    #arg="load "$DATA/$i" into ns:"$i
+    #echo $arg
+    #awk -v line="$arg" '/open vfb/ { print; print line; next }1' $WS/rdf4j.txt > $WS/tmp.txt
+    #cp $WS/tmp.txt $WS/rdf4j.txt
+    curl -X POST -H "Content-type: text/turtle" --data-binary @$i ${RDF4JSERVER}/repositories/vfb/statements?context=_:$i
+done
